92e45ef7e5deec9f9dc89df51aeb470fe170149f
12345star <yoansalas76@gmail.com>
2018-04-08 00:15:47 -0600

    los dos input de fecha final y fecha inicial de los reportes de venta ya esta terminada y validada que el maximo de la fecha sea la actual y el minimo el año pasado diciembre 2017 y que se carge de uno en uno en la base de datos

--- view/reportes/ventas.php
+++ view/reportes/ventas.php
@@ -26,7 +26,6 @@
 <body background="../fondo.jpg" style="width:90%;margin-left:5%;margin-top:2%">
 <?php 
 include_once '../menuView.php';
-$fecha = date('Y-m-d');
  ?>
  
  <div id="content">
@@ -36,8 +35,9 @@ $fecha = date('Y-m-d');
  	</div>
 
  	<div id="fechas">
- 		<label>Fecha Inicial</label><input type="date" id="fechainicial" autocomplete="on" value="<? echo $fecha; ?>">
- 		<label id="lblfil">Fecha Final</label><input type="date" id="fechafinal" autocomplete="on" value="<?echo $fecha?>">
+ 		<label>Fecha Inicial</label><input type="date" id="fechainicial" autocomplete="on" value="<?PHP  echo date('Y-m-d'); ?>"
+    step="1" min="2017-12-30" max="<?PHP  echo date('Y-m-d'); ?>">
+ 		<label id="lblfil">Fecha Final</label><input type="date" id="fechafinal" autocomplete="on" value="<?PHP  echo date('Y-m-d'); ?>" step="1" min="2017-12-30" max="<?PHP  echo date('Y-m-d'); ?>">
  		<input type="submit" id="procesar" value="Cargar Busqueda" onclick="buscarDatos();">
  	</div>
 
