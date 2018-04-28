260ae8c5a50674bee49fc794f54182a4dc608445
12345star <yoansalas76@gmail.com>
2018-04-28 08:41:19 -0600

    arreglando los errores de lo que es el js de productor imagen que subio brandon

--- view/productor/verImagenProductorSocioView.php
+++ view/productor/verImagenProductorSocioView.php
@@ -4,9 +4,9 @@
   <title>Productor Socio Imagen</title>
 
   <!-- CSS 
-  <script type="text/javascript" src="../../js/jquery-1.10.2.js"></script>-->
-  <script type="text/javascript" src="../../js/jquery-3.3.1.js"></script>
-  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>-->
+  <script type="text/javascript" src="../../js/jquery-1.10.2.js"></script>
+  <script type="text/javascript" src="../../js/jquery-3.3.1.js"></script>-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="../../css/imagenes/imagenesProductor.css">
   <link rel="stylesheet" type="text/css" href="../../css/distribuidor/modalImagen.css">
 
